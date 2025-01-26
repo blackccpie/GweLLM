86877f4dadfc0ad63844086723b938e40a5a857f
Albert Murienne <16404298+blackccpie@users.noreply.github.com>
2025-01-26 14:54:33 +0100

    fix : using model library test query.

--- gwellm/test_model_instruct.py
+++ gwellm/test_model_instruct.py
@@ -33,8 +33,6 @@ import torch
 # load model card
 modelcard = model_library.get_model('gemma2-2b')
 
-query = "<start_of_turn>You're a Breton assistant. Answer the following user request: Demat, gouzout a rez komz brezhoneg?<end_of_turn>\n<start_of_turn>model\n"
-
 # load tokenizer  base model
 tokenizer = AutoTokenizer.from_pretrained(modelcard.base_model_id)
 tokenizer.pad_token = tokenizer.eos_token # Most LLMs don't have a pad token by default
@@ -46,7 +44,7 @@ model.eval()
 model.config.use_cache = True
 
 with torch.no_grad():
-    inputs = tokenizer([query], add_special_tokens=True, return_tensors="pt").to("cuda")
+    inputs = tokenizer([modelcard.test_query], add_special_tokens=True, return_tensors="pt").to("cuda")
     outputs = model.generate(**inputs, max_new_tokens = 256, pad_token_id=tokenizer.pad_token_id)
     print(f"\n\nADAPTER MODEL OUTPUT:\n{tokenizer.decode(outputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)}")
 
