c65819ac1cb308de9bc69dda88ad4483c085232c
Albert Murienne <16404298+blackccpie@users.noreply.github.com>
2025-02-02 20:41:14 +0100

    feat : dataset export (arrow format).

--- goulenn/magpie_instruct_dataset_generation.py
+++ goulenn/magpie_instruct_dataset_generation.py
@@ -24,6 +24,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 import re
 
+from datasets import Dataset
+
 modelcard = 'croissantllm/CroissantLLMChat-v0.1'
 pre_query_template_with_system_prompt = "<|im_start|>user\nje suis un français, voici ma question: "
 
@@ -31,30 +33,59 @@ pre_query_template_with_system_prompt = "<|im_start|>user\nje suis un français,
 tokenizer = AutoTokenizer.from_pretrained(modelcard, use_fast=True, legacy=False)
 model = AutoModelForCausalLM.from_pretrained(modelcard, device_map='auto')
 
-pipe = pipeline('text-generation', model=model, tokenizer=tokenizer, do_sample=True, temperature=0.9, truncation=True, max_length=100, return_full_text=False)
+pipe = pipeline('text-generation', model=model, tokenizer=tokenizer, do_sample=True, temperature=0.9, truncation=True, max_length=256, return_full_text=False)
 
 def clean_question(question):
     """
-    TODO
+    Clean the question from unwanted formatting
     """
-    # remove number prefixes and optional punctuation after them
-    question = re.sub(r"^\d+[\).]?\s*", "", question)
-     # remove leading quotation marks if present
-    question = question.lstrip('"')
+    # regex explanation:
+    # ^\d+[\).]?\s* → removes leading numbers (e.g., "1)", "5.", etc.) and spaces
+    # ^["«]* → removes leading quotes (") and guillemets («)
+    question = re.sub(r'^(?:\d+[\).]?\s*)?["«]*', '', question).strip()
     # ensure the first letter is capitalized
     return question[0].upper() + question[1:]
 
-def extract_question(answer):
+def extract_question(model_output):
     """
-    TODO
+    Extract the question from model generation
     """
     try:
-        return clean_question(f"{answer.split('?')[0].strip()}?")
+        extract_question = f"{model_output.split('?')[0].strip()}?"
+        print(f"extracted: {extract_question}")
+        return clean_question(extract_question)
     except:
         print("failed extraction question from answer")
 
-for i in range(10):
-    answer = pipe(pre_query_template_with_system_prompt, chat_template=None)[0]['generated_text']
-    print(extract_question(answer))
+def extract_answer(model_output):
+    """
+    Clean the question from leading unwanted formatting
+    """
+    return f"{model_output.split('.')[0].strip()}."
+
+questions = []
+answers = []
+
+# create QA pairs iteratively
+for i in range(2):
+    q_gen = pipe(pre_query_template_with_system_prompt, chat_template=None)[0]['generated_text']
+    question = extract_question(q_gen)
+    print(f"clean: {question}")
+    print("--")
+    a_gen = pipe(f"<|im_start|>user\nrépond à la question suivante en UNE SEULE phrase: {question}<|im_end|><|im_start|>assistant\n")[0]['generated_text']
+    answer = extract_answer(a_gen)
+    print(answer)
     print("------------------------")
 
+    questions.append(question)
+    answers.append(answer)
+
+# build the dictionary
+instruct_dictionary = {
+    "input": questions,
+    "output": answers
+}
+
+# build dataset and save
+dataset = Dataset.from_dict(instruct_dictionary)
+dataset.save_to_disk("my_dataset")
