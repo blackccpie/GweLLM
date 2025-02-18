890c91ac355ddf481eb613a263a5d0ca5c3a2d4e
Albert Murienne <16404298+blackccpie@users.noreply.github.com>
2025-02-18 23:39:09 +0100

    feat : initial prototype of the translated chat.

---
+++ gallek/test_translated_chat_model_gradio.py
@@ -0,0 +1,125 @@
+# The MIT License
+
+# Copyright (c) 2025 Albert Murienne
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import gradio as gr
+
+from transformers import (
+    AutoTokenizer, 
+    AutoModelForSeq2SeqLM, 
+    AutoModelForCausalLM, 
+    BitsAndBytesConfig,
+    pipeline
+)
+
+import torch
+
+# CHAT MODEL
+
+chat_modelcard = 'google/gemma-2-2b-it'
+
+tokenizer = AutoTokenizer.from_pretrained(chat_modelcard)
+model = AutoModelForCausalLM.from_pretrained(chat_modelcard, quantization_config=BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16), device_map='auto')
+
+chat_pipeline = pipeline('text-generation', model=model, tokenizer=tokenizer, do_sample=True, temperature=0.5, truncation=True, max_length=512, return_full_text=False)
+
+# TRANSLATION MODELS
+
+fw_modelcard = "gallek-m2m100-b40"
+bw_modelcard = "kellag-m2m100-b51"
+
+fw_model = AutoModelForSeq2SeqLM.from_pretrained(fw_modelcard)
+fw_tokenizer = AutoTokenizer.from_pretrained(fw_modelcard)
+
+fw_translation_pipeline = pipeline("translation", model=fw_model, tokenizer=fw_tokenizer, src_lang='fr', tgt_lang='br', max_length=400, device="cuda")
+
+bw_model = AutoModelForSeq2SeqLM.from_pretrained(bw_modelcard)
+bw_tokenizer = AutoTokenizer.from_pretrained(bw_modelcard)
+
+bw_translation_pipeline = pipeline("translation", model=bw_model, tokenizer=bw_tokenizer, src_lang='br', tgt_lang='fr', max_length=400, device="cuda")
+
+# translation function
+def translate(text, forward: bool):
+    if forward:
+        return fw_translation_pipeline("traduis de français en breton: " + text)[0]['translation_text']
+    else:
+        return bw_translation_pipeline("treiñ eus ar galleg d'ar brezhoneg: " + text)[0]['translation_text']
+
+# answer function
+def answer(text):
+     return chat_pipeline(text, chat_template=None)[0]['generated_text']
+
+def format_prompt_with_history(message, native_chat_history):
+    # format the conversation history
+    prompt = ""
+    for interaction in native_chat_history:
+        prompt += f"<start_of_turn>{interaction['role']}\n{interaction['content']}<end_of_turn>\n"
+
+    # add the current user message
+    prompt += f"<start_of_turn>user\ntu es un assistant francophone. Répond en une seule phrase sans formattage.\n{message}<end_of_turn>\n<start_of_turn>assistant\n"
+
+    return prompt
+
+# maximum number of interactions to keep in history
+max_history_length = 3
+
+# keep a hidden model "native" language chat history
+native_chat_history = []
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(label="GweLLM Chatbot", type="messages")
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+
+    def respond(message, chat_history):
+        """
+        Handles bot response generation
+        """
+
+        global native_chat_history
+
+        fr_message = translate(message, forward=False)
+        print(f"user fr -> {fr_message}")
+
+        prompt = format_prompt_with_history(fr_message, native_chat_history)
+
+        bot_fr_message = answer(prompt)
+        print(f"bot fr -> {bot_fr_message}")
+        bot_br_message = translate( bot_fr_message, forward=True)
+        print(f"bot br -> {bot_br_message}")
+        
+        chat_history.append({"role": "user", "content": message})
+        chat_history.append({"role": "assistant", "content": bot_br_message})
+
+        native_chat_history.append({"role": "user", "content": fr_message})
+        native_chat_history.append({"role": "assistant", "content": bot_fr_message})
+
+        # limit the history length
+        if len(chat_history) > max_history_length * 2:
+            chat_history = chat_history[-max_history_length * 2:]
+            native_chat_history = native_chat_history[-max_history_length * 2:]
+
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+if __name__ == "__main__":
+    demo.launch()
